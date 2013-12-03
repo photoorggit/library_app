8b86dff74fc71dfb2262f5ebb71286ce4c7c1a75
venkataseshanka <venkat.pisupate@valuelabs.com>
2013-12-03 15:43:12 +0530

    removed unneccesary cdnjs script tags

--- app/assets/javascripts/backbone/library_header/views/TabEventsView.js
+++ app/assets/javascripts/backbone/library_header/views/TabEventsView.js
@@ -4,10 +4,18 @@ var TabEventsView = Backbone.View.extend({
     },
     render : function(event) {
         event.preventDefault();
-        $(event.currentTarget).parent().children('li')
-            .css('font-weight', 'lighter');
+        var li=$(event.currentTarget).parent().children('li');
+        li.css('font-weight', 'lighter');
         $(event.currentTarget).css('font-weight','900');
         //libContainer
+      var clickedLi = $(event.currentTarget).children().text().trim();
+             if(!clickedLi.indexOf("Stories")){
+                $(".fluidGridContainer").hide();
+                $(".libContainer").hide();
+             }else{
+                 $(".fluidGridContainer").show();
+                 $(".libContainer").show();
+             }
         return false;
     }
 });
