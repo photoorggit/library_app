3348431ce0d888d8cff0da4371289cea006e1041
venkataseshanka <venkat.pisupate@valuelabs.com>
2013-12-10 10:47:11 +0530

    modified and organised  header related code for visual effects

--- app/assets/javascripts/backbone/library_header/views/InputListView.js
+++ app/assets/javascripts/backbone/library_header/views/InputListView.js
@@ -1,6 +1,27 @@
 var InputListView = Backbone.View.extend({
+    defaults: {
+          isRotated : false
+        },
         initialize : function() {
             this.render();
+        },
+        events :{
+            "click .customfilter img" : "showDatePeopleFilter"
+        },
+        showDatePeopleFilter : function(event){
+
+        var element=$(event.currentTarget);
+            element.css("width","29");
+            if(!this.isRotated){
+                this.isRotated=true;                
+                element.addClass('rotate-90-deg');
+                element.removeClass('reset-rotate');
+            }else{
+                this.isRotated=false;                
+                element.addClass('reset-rotate');
+                element.removeClass('rotate-90-deg');
+            }
+        
         },
         render : function(event) {
             if(event)
